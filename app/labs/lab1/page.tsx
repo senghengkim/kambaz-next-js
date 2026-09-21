38d5d7eea1e4c579b64c034bf4445712fe8e4b03
Hengkim Seng <senghengkim@gmail.com>
2026-09-21 18:55:19 -0400

    Update student name and git config

--- app/labs/lab1/page.tsx
+++ app/labs/lab1/page.tsx
@@ -3,7 +3,12 @@ export default function Lab1() {
     return (
         <div id="wd-lab1">
             <h2>Lab 1</h2>
-
+            <h3>Hengkim Seng</h3>
+            <h4>Section: CS5610: 01</h4>
+            <a id="wd-github" href="https://github.com/senghengkim/kambaz-next-js">
+                Github Repository
+            </a>
+            {/*Empty for now */}
         </div>
     );
 }
